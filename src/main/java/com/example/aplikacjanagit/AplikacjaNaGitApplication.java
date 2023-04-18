cebd1081176e24961cbe4f25b9e2f3bb8ed686a8
karokal922 <73292943+karokal922@users.noreply.github.com>
2023-04-18 11:45:17 +0200

    za

--- src/main/java/com/example/aplikacjanagit/AplikacjaNaGitApplication.java
+++ src/main/java/com/example/aplikacjanagit/AplikacjaNaGitApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AplikacjaNaGitApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AplikacjaNaGitApplication.class, args);
     }
-
 }
